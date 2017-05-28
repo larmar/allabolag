483495260404f89bf40ce480def886ca42511c97
Riyaj Pathan <rjpathan19@gmail.com>
2017-05-28 18:53:16 +0530

    allow only numbers 0-9 for Orgnr

--- contact_allabolag/models/res_partner.py
+++ contact_allabolag/models/res_partner.py
@@ -12,6 +12,13 @@ from odoo.exceptions import ValidationError
 import logging
 _logger = logging.getLogger(__name__)
 
+def check_digits(number = False):
+    if number:
+        for res in str(number):
+            if ord(res) not in (48, 49, 50, 51, 52, 53, 54, 55, 56, 57):
+                return False
+    return True
+
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
@@ -38,6 +45,9 @@ class ResPartner(models.Model):
                 if len(str(orgnr)) != 10:
                     raise ValidationError('Orgnr must be 10 digits number.\nYou entered {} digits: {}'.format(len(str(orgnr)), orgnr))
 
+                if not check_digits(str(orgnr)):
+                    raise ValidationError('Orgnr must be 10 digits number between 0-9.\nYou entered: {}'.format(orgnr))
+
                 orgnr_search = self.search([('orgnr', '=', orgnr)])
                 if orgnr_search:
                     raise ValidationError('Orgnr {} already exists for another company.\nCompany: {}'.format(orgnr, orgnr_search[0].name))
@@ -53,6 +63,10 @@ class ResPartner(models.Model):
             if orgnr: 
                 if len(str(orgnr)) != 10:
                     raise ValidationError('Orgnr must be 10 digits number.\nYou entered {} digits: {}'.format(len(str(orgnr)), orgnr))
+
+                if not check_digits(str(orgnr)):
+                    raise ValidationError('Orgnr must be 10 digits number between 0-9.\nYou entered: {}'.format(orgnr))
+
                 orgnr_search = self.search([('orgnr', '=', orgnr)])
                 if orgnr_search and orgnr_search[0].id != self.id:
                     raise ValidationError('Orgnr {} already exists for another company.\nCompany: {}'.format(orgnr, orgnr_search[0].name))
@@ -70,6 +84,8 @@ class ResPartner(models.Model):
                     raise ValidationError('Orgnr {} already exists for another company.\nCompany: {}'.format(self.orgnr, orgnr_search[0].name))
             if len(str(self.orgnr)) != 10:
                 raise ValidationError('Orgnr must be 10 digits number.\nYou entered {} digits: {}'.format(len(str(self.orgnr)), self.orgnr))
+            if not check_digits(str(self.orgnr)):
+                raise ValidationError('Orgnr must be 10 digits number between 0-9.\nYou entered: {}'.format(self.orgnr))
 
 
     @api.multi
